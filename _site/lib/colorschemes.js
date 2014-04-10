49ab1641e3aca29f4ba76eaab583e71bd02862bd
Casey Kuhlman <caseykuhlman@watershedlegal.com>
2014-04-10 10:50:30 +0200

    Site updated at 2014-04-10 08:50:30 UTC

--- _site/lib/colorschemes.js
+++ _site/lib/colorschemes.js
@@ -1,9 +1,9 @@
 function blueGreen(d) {
-    return d == 1 ? '#F0F9E8' :
-           d == 2 ? '#BAE4BC' :
-           d == 3 ? '#7BCCC4' :
-           d == 4 ? '#43A2CA' :
-           d == 5 ? '#0868AC' :
-           d == 'border' ? '#444' :
-                    '#F0F9E8' ;
+    return d == 1        ? '#F0F9E8' :
+           d == 2        ? '#BAE4BC' :
+           d == 3        ? '#7BCCC4' :
+           d == 4        ? '#43A2CA' :
+           d == 5        ? '#0868AC' :
+           d == 'border' ? '#444444' :
+                           '#F0F9E8' ;
 }
